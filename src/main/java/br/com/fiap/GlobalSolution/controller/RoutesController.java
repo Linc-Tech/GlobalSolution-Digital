6ec2a8e858de0f576901c63b7acac14e57a49853
Gabriel Lucas <61299646+gabriel-lucas-sl@users.noreply.github.com>
2021-11-10 01:26:21 -0300

    wip

--- src/main/java/br/com/fiap/GlobalSolution/controller/RoutesController.java
+++ src/main/java/br/com/fiap/GlobalSolution/controller/RoutesController.java
@@ -1,10 +1,8 @@
 package br.com.fiap.GlobalSolution.controller;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 
 @Controller
 @RequestMapping("v1/routes")
