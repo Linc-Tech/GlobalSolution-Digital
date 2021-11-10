57a7bab0815bad99e0f4355d2437296d90dd534b
Gabriel Lucas <61299646+gabriel-lucas-sl@users.noreply.github.com>
2021-11-10 20:37:04 -0300

    wip

--- src/main/java/br/com/fiap/GlobalSolution/controller/RoutesController.java
+++ src/main/java/br/com/fiap/GlobalSolution/controller/RoutesController.java
@@ -13,4 +13,34 @@ public class RoutesController {
         return "index";
     }
 
+    @GetMapping("/login")
+    public String openLoginPage() {
+        return "pages/onboarding/login";
+    }
+
+    @GetMapping("/registration")
+    public String openRegistrationPage() {
+        return "pages/onboarding/registration";
+    }
+
+    @GetMapping("/ongs")
+    public String openDonationPage() {
+        return "pages/donate/donate";
+    }
+
+    @GetMapping("/home")
+    public String openHomePage() {
+        return "pages/home/ong";
+    }
+
+    @GetMapping("/home/donations")
+    public String openConfirmDonationPage() {
+        return "pages/home/donations/donations";
+    }
+
+    @GetMapping("/home/settings")
+    public String openSettingsPage() {
+        return "pages/home/settings/settings";
+    }
+
 }
